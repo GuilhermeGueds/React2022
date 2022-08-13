63beb42e67775b83db998eb4fb181c2732e24fcc
GuilhermeGueds <guilhermeguedes.cc@gmail.com>
2022-08-13 18:28:28 -0300

    feat: created comment component

---
+++ 01-fundamentos-nodejs/src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="http://github.com/GuilhermeGueds.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Guedes</strong>
+              <time title="11 de maio" dateTime="2022-05-11">
+                Cerca de 1 hora atrás
+              </time>
+            </div>
+            <button title="Deletar Comentário">
+              <Trash />
+            </button>
+          </header>
+          <p>Muito bom parabens</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir . <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
