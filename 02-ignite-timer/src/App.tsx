4e3a9fbeaee1686ffc6a3f26ce29646f4a693cb1
GuilhermeGueds <guilhermeguedes.cc@gmail.com>
2022-08-20 15:16:16 -0300

    init React Router DOM

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,18 +1,19 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { Router } from "./components/Router";
+import { BrowserRouter } from "react-router-dom";
+
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 // import { ButtonContainer as Button } from "./components/Button";
 
 export function App() {
-	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Button variant="primary" />
-			<Button variant="secundary" />
-			<Button variant="danger" />
-			<Button variant="sucess" />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
-			<GlobalStyle />
-		</ThemeProvider>
-	);
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
